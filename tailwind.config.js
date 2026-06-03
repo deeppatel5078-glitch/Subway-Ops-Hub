e18e0b8acea2101ab01c6488049b6515519aad5e
deeppatel5078-glitch <deeppatel5078@gmail.com>
2026-06-03 12:18:17 +1000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        subway: {
+          green: '#008C15',
+          yellow: '#FFC600',
+          dark: '#1a1a1a',
+        }
+      }
+    }
+  },
+  plugins: []
+}
